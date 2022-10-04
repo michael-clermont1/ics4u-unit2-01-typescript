4ff5f6d1a45b4a3b6de8e6638fdfe0981a81d9ff
Michael Clermont <michael.clermont@mths.ca>
2022-10-04 13:26:54 +0000

    added stack program

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+/**
+ * This program uses the MrCoxallStack
+ *
+ * By:      Michael Clermont
+ * Version: 1.0
+ * Since:   2022-9-24
+ */
+
+import Stack from './stack'
+import promptSync from 'prompt-sync'
+
+const myNewStack = new Stack()
+
+const prompt = promptSync()
+const someNumberString = prompt('Enter an integer: ')
+const someNumberInt = parseInt(someNumberString)
+myNewStack.push(someNumberInt)
+myNewStack.showStack()
---
+++ src/stack.ts
@@ -0,0 +1,21 @@
+/**
+ * This class creates an integer stack
+ *
+ * By:      Michael Clermont
+ * Version: 1.0
+ * Since:   2022-10-3
+ */
+
+class Stack {
+  stackArray: number[] = []
+
+  push(pushedNumber: number): void {
+    // add a number to the array
+    this.stackArray.push(pushedNumber)
+  }
+
+  showStack(): void {
+    console.log(this.stackArray)
+  }
+}
+export = Stack
